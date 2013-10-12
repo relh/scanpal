14dc23efdff2ce90c3445f75d84de21c81c800f4
Alejandro Newell <alejandro.newell@gmail.com>
2013-10-12 15:06:00 -0400

    voice

--- src/com/ampvita/scanpal/VoiceActivity.java
+++ src/com/ampvita/scanpal/VoiceActivity.java
@@ -3,20 +3,72 @@ package com.ampvita.scanpal;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import java.util.ArrayList;
+import java.util.regex.*;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class VoiceActivity extends Activity {
 
+	protected static final int RESULT_SPEECH = 1;
+	
+	private ImageButton btnSpeak;
+	private TextView txtText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_voice);
+		
+		txtText = (TextView) findViewById(R.id.txtText);
+		btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+		btnSpeak.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+			
+				Intent intent = new Intent(
+						RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+				intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+				
+				try {
+					startActivityForResult(intent, RESULT_SPEECH);
+					txtText.setText("");
+				} catch (ActivityNotFoundException a) {
+					Toast t = Toast.makeText(getApplicationContext(),
+                            "Opps! Your device doesn't support Speech to Text",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+				}
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.voice, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		switch (requestCode) {
+		case RESULT_SPEECH: {
+			if (resultCode == RESULT_OK && null != data) {
+				ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+				txtText.setText(text.get(0));
+				
+				//Pattern money = Pattern.compile('\$\d+');
+				
+			}
+		}
+		}
+	}
+	
 }
