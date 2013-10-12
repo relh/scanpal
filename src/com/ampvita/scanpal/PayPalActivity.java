b5bb23741019f2c74f1dae4125bfcca673bbe651
Richard Higgins <relh@terpmail.umd.edu>
2013-10-12 14:34:33 -0400

    testing 1

--- src/com/ampvita/scanpal/PayPalActivity.java
+++ src/com/ampvita/scanpal/PayPalActivity.java
@@ -2,6 +2,7 @@ package com.ampvita.scanpal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class PayPalActivity extends Activity {
 
@@ -9,5 +10,12 @@ public class PayPalActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pay_pal);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.qr, menu);
+		return true;
 	}
 }
