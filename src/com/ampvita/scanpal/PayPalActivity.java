9c028a631009e1b423fe89cac35065d20b6c109b
Wajahat Siddiqui <agentwaj@gmail.com>
2013-10-12 14:32:23 -0400

    Clean up PayPalActivity.

--- src/com/ampvita/scanpal/PayPalActivity.java
+++ src/com/ampvita/scanpal/PayPalActivity.java
@@ -1,8 +1,7 @@
 package com.ampvita.scanpal;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class PayPalActivity extends Activity {
 
@@ -11,12 +10,4 @@ public class PayPalActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pay_pal);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.pay_pal, menu);
-		return true;
-	}
-
 }
