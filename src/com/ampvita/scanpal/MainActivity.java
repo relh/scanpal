0faa676590241138e16e07948172297a60194ccd
Wajahat Siddiqui <agentwaj@gmail.com>
2013-10-12 14:23:59 -0400

    Clean MainActivity.

--- src/com/ampvita/scanpal/MainActivity.java
+++ src/com/ampvita/scanpal/MainActivity.java
@@ -1,10 +1,10 @@
 package com.ampvita.scanpal;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -13,34 +13,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Button a = (Button)findViewById(R.id.button1);
-		a.setOnClickListener(new View.OnClickListener() {
-		@Override
-		public void onClick(View arg0) {
-			Intent myIntent = new Intent(MainActivity.this, VoiceActivity.class);
-			MainActivity.this.startActivity(myIntent); } });
-		
-		Button b = (Button)findViewById(R.id.button2);
-		b.setOnClickListener(new View.OnClickListener() {
-		@Override
-		public void onClick(View arg0) {
-			Intent myIntent = new Intent(MainActivity.this, VoiceActivity.class);
-			MainActivity.this.startActivity(myIntent); } });
-		
-		Button c = (Button)findViewById(R.id.button3);
-		c.setOnClickListener(new View.OnClickListener() {
-		@Override
-		public void onClick(View arg0) {
-			Intent myIntent = new Intent(MainActivity.this, VoiceActivity.class);
-			MainActivity.this.startActivity(myIntent); } });
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		((Button)findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, PayPalActivity.class)); 
+			} 
+		});
 
+		((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, QRActivity.class)); 
+			} 
+		});
+
+		((Button)findViewById(R.id.button3)).setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, VoiceActivity.class)); 
+			} 
+		});
+	}
 }
